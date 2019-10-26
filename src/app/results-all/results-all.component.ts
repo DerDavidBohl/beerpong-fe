7f1f8892cbaf7af11a9fe307428a27e66a6f2110
BuildTools <unconfigured@null.spigotmc.org>
2019-10-26 21:47:34 +0200

    Implemented Season Filter in Ranking

--- src/app/results-all/results-all.component.ts
+++ src/app/results-all/results-all.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort } from '@angular/material';
-import { RankingService, AthleteRanking } from '../ranking.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTableDataSource, MatSort } from "@angular/material";
+import { RankingService, AthleteRanking } from "../ranking.service";
+import { SeasonService, SeasonWithId } from "../season.service";
+import { Observable } from "rxjs";
+import { FormControl } from "@angular/forms";
+import { startWith, map, ignoreElements } from "rxjs/operators";
 
 export interface PeriodicElement {
   name: string;
@@ -10,27 +14,79 @@ export interface PeriodicElement {
 }
 
 @Component({
-  selector: 'app-results-all',
-  templateUrl: './results-all.component.html',
-  styleUrls: ['./results-all.component.css']
+  selector: "app-results-all",
+  templateUrl: "./results-all.component.html",
+  styleUrls: ["./results-all.component.css"]
 })
 export class ResultsAllComponent implements OnInit {
-
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-  displayedColumns: string[] = ['position', 'name', 'victories', 'ownHits', 'hostileHits'];
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  displayedColumns: string[] = [
+    "position",
+    "name",
+    "games",
+    "victories",
+    "defeats",
+    "ownHits",
+    "hostileHits"
+  ];
   dataSource;
-  
-  constructor(private rankingService: RankingService) { }
+
+  selectedSeason: SeasonWithId = null;
+  allSeasons: SeasonWithId[];
+  filteredSeasons: Observable<SeasonWithId[]>;
+  stateCtrl = new FormControl();
+
+  constructor(
+    private rankingService: RankingService,
+    private seasonService: SeasonService
+  ) {}
 
   ngOnInit() {
-    this.rankingService.getAllAthleteRankings().subscribe(rankings => {
+    this.updateRanking();
+
+    this.seasonService.getAll().subscribe(seasons => {
+      this.allSeasons = seasons;
+
+      this.filteredSeasons = this.stateCtrl.valueChanges.pipe(
+        startWith(""),
+        map(season =>
+          season ? this._filterStates(season) : this.allSeasons.slice()
+        )
+      );
+
+      this.stateCtrl.valueChanges.subscribe(value => {
+        this.selectedSeason = value;
+        this.updateRanking();
+      });
+    });
+  }
+
+  updateRanking() {
+    let seasonId = null;
+
+    if (this.selectedSeason) {
+      seasonId = this.selectedSeason.id;
+    }
+
+    this.rankingService.getAllAthleteRankings(seasonId).subscribe(rankings => {
       this.dataSource = new MatTableDataSource(rankings);
       this.dataSource.sort = this.sort;
     });
   }
 
+  displayFn(season?: SeasonWithId): string | undefined {
+    return season ? season.name : undefined;
+  }
+
+  private _filterStates(value: string): SeasonWithId[] {
+    let filterValue = value.toLowerCase();
+
+    return this.allSeasons.filter(
+      season => season.name.toLowerCase().indexOf(filterValue) === 0
+    );
+  }
+
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
